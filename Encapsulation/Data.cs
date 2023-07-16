6722169f136f7985ad491083886ff77e17094a5b
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2023-07-16 22:55:28 +0700

    Add files via upload

---
+++ Encapsulation/Data.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace uas2
+{
+	public class Data
+	{
+		private string NAMA;
+        protected int UMUR;
+		internal string PEKERJAAN;
+        private string SAKU;
+
+		public string Nama
+		{
+			get { return NAMA; }
+			set { NAMA = value; }
+		}
+
+        public int Umur
+        {
+            get { return UMUR; }
+            set { UMUR = value; }
+        }
+
+        public string Pekerjaan
+        {
+            get { return PEKERJAAN; }
+            set { PEKERJAAN = value; }
+        }
+
+        public string Saku
+        {
+            get { return SAKU; }
+            set { SAKU = value; }
+        }
+
+        public void TampilanDataDiri()
+		{
+            Console.WriteLine("======================================");
+            Console.WriteLine("\tDATA DIRI");
+            Console.WriteLine("======================================");
+            Console.WriteLine();
+
+            Console.WriteLine("Nama\t\t: {0}", Nama);
+            Console.WriteLine("Umur\t\t: {0}", Umur);
+            Console.WriteLine("Pekerjaan\t: {0}", Pekerjaan);
+            Console.WriteLine("Uang saku\t: {0}", Saku);
+
+            Console.WriteLine();
+            Console.WriteLine("Tekan sembarang tombol untuk kembali ke menu..");
+            Console.ReadKey();
+            Console.Clear();
+        }
+	}
+}
